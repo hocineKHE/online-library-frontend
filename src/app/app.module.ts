427c51e31f1e74f30c5e9ee5f0ba62ad22d9a973
Hocine KHEDDADJI <khedadjihoucine@gmail.com>
2021-01-13 14:09:02 +0100

    deature 3 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { BooksComponent } from './books/books.component';
 import {HttpClientModule} from "@angular/common/http";
 import {BookService} from "./books/book-services/book.service";
 import { BooksGridComponent } from './books/books-grid/books-grid.component';
+import {RouterModule ,Routes} from "@angular/router";
+
+const routes: Routes =[
+  {path: "books", component: BooksComponent},
+  {path: "category/:id", component: BooksComponent},
+  {path: "",redirectTo: "/books", pathMatch: 'full'}
+
+
+]
 
 @NgModule({
   declarations: [
